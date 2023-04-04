7fe9bb5099cd42d284b1285321c92f3ab892950d
Jiingz <129873277+RiposteSharp@users.noreply.github.com>
2023-04-04 17:50:36 +0200

    Added Convex Hull mathematics.

---
+++ Math/Geometry.cs
@@ -0,0 +1,13 @@
+﻿using SharpDX;
+using SharpDX.Direct3D9;
+
+namespace Riposte.SDK.Math
+{
+    /// <summary>
+    ///     Geometry math class, contains geometry calculations.
+    /// </summary>
+    public static class Geometry
+    {
+    }
+}
+
