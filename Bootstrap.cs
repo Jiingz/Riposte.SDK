60f00583420843af5b2920a2683fc842aae43c5e
Jiingz <129873277+RiposteSharp@users.noreply.github.com>
2023-05-08 22:29:31 +0200

    Create Bootstrap.cs

---
+++ Bootstrap.cs
@@ -0,0 +1,19 @@
+﻿namespace Riposte.SDK
+{
+    public static class Bootstrap
+    {
+        private static bool _initialized;
+
+        public static bool Init()
+        {
+            if (_initialized)
+                return false;
+
+
+
+            _initialized = true;
+
+            return true;
+        }
+    }
+}
